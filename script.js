d837f9bca472c41c1436bab0c91522431c978b24
wsyianis <ianisfromentin55@gmail.com>
2026-05-13 13:17:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,242 +1,324 @@
-// script.js
 const ghosts = [
+
     {
         name: "Banshee",
-        characteristics: "Cible un joueur au hasard au début de la partie, et change de cible une fois qu'il meurt ou quitte la partie.",
-        hunt: "Chasse uniquement un joueur ciblé.",
-        speed: "1,7 m/s (base), 2,8 m/s (max)",
-        evidence: "Empreintes, Orbes, D.O.T.S."
+        characteristics: "Cible un joueur précis et reste focalisée sur lui.",
+        hunt: "Chasse uniquement sa cible.",
+        speed: "1,7 m/s • 2,8 m/s max",
+        evidence: "Empreintes • Orbes • D.O.T.S",
+        theme: "banshee",
+        special: "Cible unique"
     },
+
     {
         name: "Démon",
-        characteristics: "Le délai minimum entre 2 chasses normales est de 20 secondes (au lieu de 25). L'encens l'empêche de chasser pendant 60 secondes (au lieu de 90).",
-        hunt: "Chasse fréquemment et à des niveaux de santé plus élevés.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Spirit Box, Écriture, EMF 5."
+        characteristics: "Très agressif et chasse souvent.",
+        hunt: "20 sec entre les chasses.",
+        speed: "1,5 m/s • 2,5 m/s max",
+        evidence: "Spirit Box • Écriture • EMF 5",
+        theme: "demon",
+        special: "Encens : 60 sec"
     },
+
     {
         name: "Yurei",
-        characteristics: "En utilisant son pouvoir, il enlève 15% de santé mentale aux joueurs à moins de 7,5 mètres de lui et ferme une porte de sa pièce entièrement.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Écriture, D.O.T.S."
+        characteristics: "Retire de la santé mentale et ferme les portes.",
+        hunt: "50% de santé mentale.",
+        speed: "1,5 m/s • 2,5 m/s max",
+        evidence: "Orbes • Écriture • D.O.T.S",
+        theme: "yurei",
+        special: "Drain mental"
     },
+
     {
         name: "Fantôme",
-        characteristics: "N'apparaît pas sur les photos et celles-ci ne sont pas brouillées. Clignotement lent, il est invisible pendant un minimum de 1 seconde contre 0,3 à 1 seconde pour les autres entités.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,7 m/s (base), 3,0 m/s (max)",
-        evidence: "Orbes, Spirit Box, EMF 5."
+        characteristics: "Invisible plus longtemps pendant les chasses.",
+        hunt: "50% de santé mentale.",
+        speed: "1,7 m/s • 3,0 m/s max",
+        evidence: "Orbes • Spirit Box • EMF 5",
+        theme: "phantom",
+        special: "peu visible"
     },
+
     {
         name: "Spectre",
-        characteristics: "Ne peut pas marcher dans le sel et ne laisse donc pas d'empreintes de pas. Peut se téléporter vers un joueur aléatoire.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, EMF 5, D.O.T.S."
+        characteristics: "Ne marche pas dans le sel et peut se téléporter.",
+        hunt: "Téléportation fréquente.",
+        speed: "1,5 m/s • 2,5 m/s max",
+        evidence: "Orbes • EMF 5 • D.O.T.S",
+        theme: "spectre",
+        special: "Tp"
     },
+
     {
         name: "Esprit",
-        characteristics: "L'encens l'empêche de chasser pendant 180 secondes (au lieu de 90).",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,7 m/s (base), 2,8 m/s (max)",
-        evidence: "Spirit Box, Écriture, EMF 5."
+        characteristics: "Très sensible à l'encens.",
+        hunt: "180 sec d'arrêt avec encens.",
+        speed: "1,7 m/s • 2,8 m/s max",
+        evidence: "Spirit Box • Écriture • EMF 5",
+        theme: "spirit",
+        special: "Encens : 180 sec"
     },
+
     {
         name: "Goryo",
-        characteristics: "Ne peut pas changer de pièce favorite. Les D.O.T.S. ne sont visibles qu'à la caméra et si personne ne se trouve dans sa pièce.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 3,0 m/s (max)",
-        evidence: "Orbes, Écriture, D.O.T.S."
+        characteristics: "Les D.O.T.S sont visibles uniquement à la caméra.",
+        hunt: "Reste souvent dans sa pièce.",
+        speed: "1,5 m/s • 3,0 m/s max",
+        evidence: "EMF 5 • Empreintes • D.O.T.S",
+        theme: "goryo",
+        special: "Caméra"
     },
+
     {
         name: "Hantu",
-        characteristics: "Ne peut pas allumer le disjoncteur. Il n'accélère pas à la vue du joueur.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Spirit Box, EMF 5."
+        characteristics: "Plus rapide dans le froid.",
+        hunt: "N'accélère pas à la vue.",
+        speed: "1,4 → 2,7 m/s",
+        evidence: "Empreintes • Orbes • Température glaciale",
+        theme: "hantu",
+        special: "Froid = vitesse"
     },
+
     {
         name: "Djinn",
-        characteristics: "Ne peut pas éteindre directement le disjoncteur. Peut drainer 25% de santé mentale.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "2,0 m/s (base), 3,0 m/s (max)",
-        evidence: "Orbes, Spirit Box, D.O.T.S."
+        characteristics: "Très rapide avec le courant.",
+        hunt: "Peut drainer 25% de santé mentale.",
+        speed: "2,5 m/s",
+        evidence: "EMF 5 • Empreintes • Spirit Box",
+        theme: "djinn",
+        special: "le Courant"
     },
+
     {
         name: "Revenant",
-        characteristics: "Plus lent, mais plus rapide quand il voit une victime.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,0 m/s (base), 3,0 m/s (max)",
-        evidence: "Orbes, Écriture, EMF 5."
+        characteristics: "Très lent sans vision mais ultra rapide quand il voit quelqu'un.",
+        hunt: "Extrêmement dangereux.",
+        speed: "1 m/s • 3 m/s",
+        evidence: "Orbes • Écriture • Température glaciale",
+        theme: "revenant",
+        special: "Ultra rapide"
     },
+
     {
         name: "Ombre",
-        characteristics: "Évite de chasser si plusieurs joueurs sont présents.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Écriture, D.O.T.S."
+        characteristics: "Très timide avec plusieurs joueurs.",
+        hunt: "Interagit très peu.",
+        speed: "1,5 m/s",
+        evidence: "EMF 5 • Écriture • Température glaciale",
+        theme: "shade",
+        special: "Très discret"
     },
+
     {
         name: "Oni",
-        characteristics: "A tendance à beaucoup interagir avec les joueurs.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 3,0 m/s (max)",
-        evidence: "Orbes, Spirit Box, EMF 5."
+        characteristics: "Très actif et visible plus longtemps.",
+        hunt: "Fait énormément d'événements.",
+        speed: "1,7 m/s",
+        evidence: "EMF 5 • Spirit Box • D.O.T.S",
+        theme: "oni",
+        special: "clignotement"
     },
+
     {
         name: "Cauchemar",
-        characteristics: "Ne peut pas allumer les lumières.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Spirit Box, EMF 5."
+        characteristics: "Éteint les lumières instantanément.apres les avoir allumer",
+        speed: "1,5 m/s",
+        hunt: "NOP",
+        evidence: "Orbes • Spirit Box • Écriture",
+        theme: "mare",
+        special: "lumiere off"
     },
+
     {
         name: "Jumeaux",
-        characteristics: "Peut faire une double interaction.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Écriture, D.O.T.S."
+        characteristics: "Peut faire deux interactions éloignées.",
+        hunt: "Deux vitesses possibles.",
+        speed: "1,5 • 1,9 m/s",
+        evidence: "EMF 5 • Spirit Box • Température glaciale",
+        theme: "twins",
+        special: "Double interaction"
     },
+
     {
-        name: "Le Mimic",
-        characteristics: "Copie une autre entité et ses caractéristiques.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Spirit Box, EMF 5."
+        name: "Le Mimique",
+        characteristics: "Copie les capacités des autres fantômes.",
+        hunt: "Comportement totalement variable.",
+        speed: "Variable",
+        evidence: "Empreintes • Spirit Box • Température glaciale",
+        theme: "mimic",
+        special: "Faux orbes"
     },
+
     {
         name: "Thaye",
-        characteristics: "Vieillit avec le temps.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Écriture, D.O.T.S."
+        characteristics: "Vieillit progressivement.",
+        hunt: "Très agressif jeune.",
+        speed: "1 → 2,7 m/s",
+        evidence: "Orbes • Écriture • D.O.T.S",
+        theme: "thaye",
+        special: "Vieillit"
     },
+
     {
         name: "Onryo",
-        characteristics: "Tente une chasse après avoir soufflé 3 flammes.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Spirit Box, EMF 5."
+        characteristics: "Déteste les flammes.",
+        hunt: "Chasse après 3 flammes souffler",
+        speed: "1,5 m/s",
+        evidence: "Orbes • Spirit Box • Température glaciale",
+        theme: "onryo",
+        special: "bougie"
     },
+
     {
         name: "Obake",
         characteristics: "Peut changer de forme.",
-        hunt: "Chasse à partir de 50 % de santé.",
-        speed: "1,5 m/s (base), 2,5 m/s (max)",
-        evidence: "Orbes, Écriture, D.O.T.S."
+        hunt: "peu changer de skin",
+        speed: "1,7 m/s",
+        evidence: "EMF 5 • Empreintes • Orbes",
+        theme: "obake",
+        special: "Transformation"
     },
+
     {
-       name: "Gallu",
-       characteristics: "Possède 3 états : Calme, Normal et Énervé. Change d’état avec le sel, l’encens et les crucifix. Plus actif et dangereux en état Énervé.",
-       hunt: "60% (énervé), 50% (normal), 40% (calme)",
-       speed: "1,36 m/s (calme), 1,70 m/s (normal), 1,96 m/s (énervé)",
-       evidence: "EMF 5, Ultraviolet, Spirit Box"
-    },
-    {
-       name: "Obambo",
-       characteristics: "Alterne entre Calme et Énervé toutes les 2 minutes. Très actif énervé, très passif calme.",
-       hunt: "65% (énervé), 10% (calme)",
-       speed: "1,45 m/s (calme), 1,96 m/s (énervé)",
-       evidence: "D.O.T.S, Écriture, Ultraviolet"
-    },
-    {
-      name: "La Dayan",
-      characteristics: "Fantôme féminin détectant les mouvements dans un rayon de 10m. Plus rapide si un joueur bouge près d’elle.",
-      hunt: "65% si mouvement, 45% immobile, sinon 50%",
-      speed: "1,2 m/s (lent), 1,7 m/s (base), 2,25 m/s (rapide)",
-      evidence: "EMF 5, Orbes, Spirit Box"
-     },
-     {
         name: "Deogen",
         characteristics: "Sait toujours où sont les joueurs.",
-        hunt: "Impossible de se cacher.",
-        speed: "3 m/s loin • 0,4 m/s proche",
-        evidence: "DOTS • Écriture • Spirit Box"
+        hunt: "Très rapide loin mais lent proche.",
+        speed: "3 m/s → 0,4 m/s",
+        evidence: "DOTS • Écriture • Spirit Box",
+        theme: "deogen",
+        special: "cache cache ?"
     },
+
     {
         name: "Moroï",
-        characteristics: "Devient plus rapide quand la santé mentale baisse.",
-        hunt: "50%",
+        characteristics: "Maudit les joueurs avec la Spirit Box.",
+        hunt: "l'encens le paralyse plus longtemps",
         speed: "1,5 → 3,71 m/s",
-        evidence: "Écriture • Spirit Box • Température glaciale"
+        evidence: "Écriture • Spirit Box • Température glaciale",
+        theme: "moroi",
+        special: "Malédiction"
     },
+
     {
         name: "Myling",
         characteristics: "Très silencieux pendant les chasses.",
-        hunt: "50%",
-        speed: "1,7 m/s",
-        evidence: "Écriture • EMF 5 • Ultraviolet"
+        hunt: "on peux le voir mais ne pas l entendre",
+        speed: "1,7 • 2,8 m/s",
+        evidence: "Écriture • EMF 5 • Ultraviolet",
+        theme: "myling",
+        special: "Pas silencieux"
     },
+
     {
         name: "Poltergeist",
-        characteristics: "Peut lancer énormément d'objets.",
+        characteristics: "Lance énormément d'objets.",
         hunt: "Très actif avec les objets.",
-        speed: "1,7 m/s",
-        evidence: "Écriture • Spirit Box • Ultraviolet"
+        speed: "1,7 • 2,8 m/s",
+        evidence: "Écriture • Spirit Box • Ultraviolet",
+        theme: "polter",
+        special: "Objets volants"
     },
+
     {
         name: "Raiju",
-        characteristics: "Plus rapide près des équipements électroniques.",
-        hunt: "65% près des appareils actifs",
-        speed: "2,5 m/s",
-        evidence: "DOTS • EMF 5 • Orbes"
-     }
-     ];
+        characteristics: "Devient rapide près des appareils électroniques.",
+        hunt: "coeur bass boosted tres rapide avec lampe torche",
+        speed: "1,7 • 2,5 • 2,8 m/s",
+        evidence: "DOTS • EMF 5 • Orbes",
+        theme: "raiju",
+        special: "Électronique"
+    },
+
+    {
+        name: "Gallu",
+        characteristics: "Possède plusieurs états émotionnels.",
+        hunt: "ne marche pas dans le sel enervé",
+        speed: "1,36 → 1,96 m/s",
+        evidence: "EMF 5 • UV • Spirit Box",
+        theme: "gallu",
+        special: "3 états"
+    },
+
+    {
+        name: "Obambo",
+        characteristics: "Alterne entre calme et énervé.",
+        hunt: "Très agressif énervé.ne chasse pas calme",
+        speed: "1,45 → 1,96 m/s",
+        evidence: "DOTS • Écriture • UV",
+        theme: "obambo",
+        special: "bypolaire"
+    },
+
+    {
+        name: "La Dayan",
+        characteristics: "Détecte les mouvements proches.",
+        hunt: "Plus rapide si le joueur bouge.",
+        speed: "1,2 → 2,25 m/s",
+        evidence: "EMF 5 • Orbes • Spirit Box",
+        theme: "dayan",
+        special: "pas bouger"
+    }
+
+];
+/* CONTAINER */
 
 const ghostsContainer = document.getElementById('ghosts-container');
 
+/* ===== OVERLAY + POPUP ===== */
+const overlay = document.createElement("div");
+overlay.id = "overlay";
+document.body.appendChild(overlay);
+
+const popup = document.createElement("div");
+popup.classList.add("popup");
+document.body.appendChild(popup);
+
+/* ===== OUVRIR POPUP ===== */
+function openPopup(ghost) {
+
+    popup.innerHTML = `
+        <h2>${ghost.name}</h2>
+
+        <p><strong>Caractéristiques :</strong> ${ghost.characteristics}</p>
+        <p><strong>Chasse :</strong> ${ghost.hunt}</p>
+        <p><strong>Vitesse :</strong> ${ghost.speed}</p>
+        <p><strong>Preuves :</strong> ${ghost.evidence}</p>
+
+        <button class="close-btn">Fermer</button>
+    `;
+
+    popup.classList.add("active");
+    overlay.classList.add("active");
+    document.body.classList.add("modal-open");
+
+    popup.querySelector(".close-btn").onclick = closePopup;
+}
+
+/* ===== FERMER POPUP ===== */
+function closePopup() {
+    popup.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("modal-open");
+}
+
+/* clic hors popup */
+overlay.addEventListener("click", closePopup);
+
+/* ===== CREATION CARTES ===== */
 ghosts.forEach(ghost => {
     const card = document.createElement('div');
     card.classList.add('card');
 
-    const title = document.createElement('h2');
-    title.textContent = ghost.name;
+    card.innerHTML = `
+        <h2>${ghost.name}</h2>
+        <p><strong>Caractéristiques :</strong> ${ghost.characteristics}</p>
+        <p><strong>Chasse :</strong> ${ghost.hunt}</p>
+        <p><strong>Vitesse :</strong> ${ghost.speed}</p>
+        <p><strong>Preuves :</strong> ${ghost.evidence}</p>
+    `;
 
-    const characteristics = document.createElement('p');
-    characteristics.innerHTML = `<strong>Caractéristiques :</strong> ${ghost.characteristics}`;
-
-    const hunt = document.createElement('p');
-    hunt.innerHTML = `<strong>Chasse :</strong> ${ghost.hunt}`;
-
-    const speed = document.createElement('p');
-    speed.innerHTML = `<strong>Vitesse :</strong> ${ghost.speed}`;
-
-    const evidence = document.createElement('p');
-    evidence.innerHTML = `<strong>Preuves :</strong> ${ghost.evidence}`;
-
-    card.appendChild(title);
-    card.appendChild(characteristics);
-    card.appendChild(hunt);
-    card.appendChild(speed);
-    card.appendChild(evidence);
+    card.addEventListener("click", () => openPopup(ghost));
 
     ghostsContainer.appendChild(card);
 });
-// AJOUTE ÇA TOUT EN BAS DE TON script.js
-
-const infoBox = document.createElement('div');
-infoBox.classList.add('info-box');
-
-infoBox.innerHTML = `
-    <h2>☠️ Infos Utiles ☠️</h2>
-
-    <p><strong>⏳ Temps entre 2 chasses :</strong> 25 secondes</p>
-
-    <p>
-        <strong>🕯️ Temps avec encens :</strong><br>
-        • 90 sec normal<br>
-        • 180 sec Esprit<br>
-        • 60 sec Démon
-    </p>
-
-    <p><strong>💨 Durée d’aveuglement de l’encens :</strong> 6 secondes</p>
-
-    <p>
-        <strong>🧠 Perte de santé mentale après un événement :</strong><br>
-        • 15% normal<br>
-        • 20% avec un Oni
-    </p>
-`;
-
-ghostsContainer.appendChild(infoBox);
